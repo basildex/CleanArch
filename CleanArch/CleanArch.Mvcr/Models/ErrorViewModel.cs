25a1431e72ce20098a3c9b93fd51b8eac79930bd
Basil Ilenikhena <basil.ilenikhena@dlhosolutions.ng>
2024-04-11 07:30:15 +0100

    Updated Presentation Layer to MVC

---
+++ CleanArch/CleanArch.Mvcr/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArch.Mvcr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
